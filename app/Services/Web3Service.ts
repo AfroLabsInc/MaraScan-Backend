a08aca4d895c8ab45967899125d34b3a4d26cd4b
Anthony <anthonynwobodo7@gmail.com>
2022-08-31 18:24:12 +0100

    :sparkles: (web3)

--- app/Services/Web3Service.ts
+++ app/Services/Web3Service.ts
@@ -1,5 +1,6 @@
 import Web3 from 'web3'
 import Env from '@ioc:Adonis/Core/Env'
+import Beneficiary from 'App/Models/Beneficiary'
 
 class Web3Service {
   private web3: Web3
@@ -10,6 +11,14 @@ class Web3Service {
     const account = await this.web3.eth.accounts.create()
 
     return account
+  }
+
+  public async checkBeneficiaryBalance(beneficiaryId: number) {
+    const beneficiary = await Beneficiary.findOrFail(beneficiaryId)
+
+    const balance = await this.web3.eth.getBalance(beneficiary.ethereumAccountAddress)
+
+    return this.web3.utils.fromWei(balance, 'ether')
   }
 }
 
