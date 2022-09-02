eef6bb875261f732adaa1da394e6333881ecf0f1
Anthony <anthonynwobodo7@gmail.com>
2022-09-02 08:26:12 +0100

    :bug: (mpesa)

--- start/routes/mpesa.ts
+++ start/routes/mpesa.ts
@@ -2,8 +2,16 @@ import Route from '@ioc:Adonis/Core/Route'
 
 export default () => {
   Route.group(() => {
-    Route.any('queue', 'MpesaIntegrationsController.queueHandler')
+    Route.group(() => {
+      Route.any('queue', 'MpesaIntegrationsController.accountBalanceQueueHandler')
 
-    Route.any('result', 'MpesaIntegrationsController.resultHandler')
-  }).prefix('mpesa/callback')
+      Route.any('result', 'MpesaIntegrationsController.accountBalanceResultHandler')
+    }).prefix('account-balance')
+
+    Route.group(() => {
+      Route.any('queue', 'MpesaIntegrationsController.b2cQueueHandler')
+
+      Route.any('result', 'MpesaIntegrationsController.b2cResultHandler')
+    }).prefix('business-to-customer')
+  }).prefix('mpesa/callbacks')
 }
