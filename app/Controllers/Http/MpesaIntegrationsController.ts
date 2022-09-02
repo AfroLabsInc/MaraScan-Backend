a2b1eb88628e6aad5122d567605f34687c5320f2
Anthony <anthonynwobodo7@gmail.com>
2022-09-02 08:17:27 +0100

    :sparkles: (mpesa )

--- app/Controllers/Http/MpesaIntegrationsController.ts
+++ app/Controllers/Http/MpesaIntegrationsController.ts
@@ -1,13 +1,27 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class MpesaIntegrationsController {
-  public async queueHandler({ request }: HttpContextContract) {
+  public async accountBalanceQueueHandler({ request }: HttpContextContract) {
     console.log(request.all())
 
     return { hello: 'world' }
   }
 
-  public async resultHandler({ request }: HttpContextContract) {
+  public async accountBalanceResultHandler({ request }: HttpContextContract) {
+    // const { Result } = request.all()
+    console.log(request.all())
+
+    // console.log(Result?.ResultParameters.ResultParameter)
+    return { hello: 'world' }
+  }
+
+  public async b2cQueueHandler({ request }: HttpContextContract) {
+    console.log(request.all())
+
+    return { hello: 'world' }
+  }
+
+  public async b2cResultHandler({ request }: HttpContextContract) {
     const { Result } = request.all()
     console.log(request.all())
 
