a5951c572d495cd319ab1d7fcbe28009620934f6
Anthony <anthonynwobodo7@gmail.com>
2022-09-05 14:30:19 +0100

    :sparkles: (admin model and auth)

---
+++ app/Models/Admin.ts
@@ -0,0 +1,36 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, beforeSave } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
+
+export default class Admin extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public email: string
+
+  @column()
+  public password: string
+
+  @column()
+  public role: string
+
+  @column()
+  public isSuperAdmin: boolean
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeSave()
+  public static async hashPassword(admin: Admin) {
+    if (admin.$dirty.password) {
+      admin.password = await Hash.make(admin.password)
+    }
+  }
+}
