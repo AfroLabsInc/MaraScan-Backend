20c831b12b4f6a13f189e50492fe63dc9dcaebf3
Anthony <anthonynwobodo7@gmail.com>
2022-09-11 00:30:40 +0100

    :bug: (fix)

--- app/Controllers/Http/CirclesController.ts
+++ app/Controllers/Http/CirclesController.ts
@@ -3,6 +3,6 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class CirclesController {
   public async notificationReceiver({ request }: HttpContextContract) {
     const body = request.body()
-    console.log('body', body)
+    console.log('body', JSON.parse(body))
   }
 }
