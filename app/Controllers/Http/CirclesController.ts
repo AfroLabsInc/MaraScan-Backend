798cf6a37f17dcb32ef336b0862e30ddb777c513
Anthony <anthonynwobodo7@gmail.com>
2022-09-11 01:24:28 +0100

    :sparkles: (notification reception)

--- app/Controllers/Http/CirclesController.ts
+++ app/Controllers/Http/CirclesController.ts
@@ -1,10 +1,27 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import DonationRequest from 'App/Models/DonationRequest'
 
 export default class CirclesController {
   public async notificationReceiver({ request }: HttpContextContract) {
-    const { notificationType, payment } = request.body()
+    const { notificationType, card, payment, transfer, settlement } = request.body()
 
-    console.log(notificationType)
-    console.log(payment)
+    if (notificationType === 'cards') {
+      // Card Creation
+      console.log(card)
+    } else if (notificationType === 'payments') {
+      console.log(payment)
+      // if the payment source is card, then it must be a donation payment
+      if (payment.source.type === 'card') {
+        const donationRequest = await DonationRequest.findByOrFail('paymentId', payment.id)
+
+        donationRequest.paymentStatus = payment.status === 'confirmed' ? 'paid' : 'failed'
+        await donationRequest.save()
+      }
+    } else if (notificationType === 'transfers') {
+      console.log(transfer)
+    } else if (notificationType === 'settlements') {
+      // Funds settlements
+      console.log(settlement)
+    }
   }
 }
