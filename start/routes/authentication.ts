b68bfc4f0d604dd9e340eb7cd376820868a06700
Anthony <anthonynwobodo7@gmail.com>
2022-09-13 10:25:59 +0100

    :bug: (logout)

--- start/routes/authentication.ts
+++ start/routes/authentication.ts
@@ -5,16 +5,19 @@ export default () => {
     Route.group(() => {
       Route.post('register', 'BeneficiaryAuthsController.register')
       Route.post('login', 'BeneficiaryAuthsController.login')
+      Route.post('logout', 'DonorAuthsController.logout')
     }).prefix('beneficiaries')
 
     Route.group(() => {
       Route.post('wallet', 'DonorAuthsController.walletAuth')
       Route.post('register', 'DonorAuthsController.register')
       Route.post('login', 'DonorAuthsController.login')
+      Route.post('logout', 'DonorAuthsController.logout')
     }).prefix('donors')
 
     Route.group(() => {
       Route.post('login', 'AdminAuthsController.login')
+      Route.post('logout', 'DonorAuthsController.logout')
     }).prefix('admin')
   }).prefix('auth')
 }
