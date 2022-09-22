c9ce9c1b153f2127c67866de09f1f4754296f537
Johnexzy <obajohn75@gmail.com>
2022-09-22 11:20:51 +0300

    :heavy_plus_sign: (commitzen, cz-emoji, eth-sig-uitls)

--- app/Services/BeneficiaryEthereumAccountService.ts
+++ app/Services/BeneficiaryEthereumAccountService.ts
@@ -32,8 +32,53 @@ class BeneficiaryEthereumAccountService extends Provider {
     return Number(balance / Math.pow(10, 6))
   }
 
-  public async withdrawFromWallet(beneficiaryId: number) {
+  /**
+   * @dev Convert amount from K
+   * @param beneficiaryId : number
+   * @param amount : number
+   */
+  public async withdrawFromWallet(beneficiaryId: number, amount: number) {
     const beneficiary = await Beneficiary.findOrFail(beneficiaryId)
+    const privateKey = await decryptText(beneficiary.ethereumAccountPrivateKey)
+
+    // implementing EIP3009
+    const contracts = new Contracts(Env.get('NETWORK'))
+
+    const maraScanOperationsContract = await contracts.marascanOperationsContract()
+
+    const dataType = {
+      types: {
+        EIP712Domain: [
+          { name: "name", type: "string" },
+          { name: "version", type: "string" },
+          { name: "chainId", type: "uint256" },
+          { name: "verifyingContract", type: "address" },
+        ],
+        TransferWithAuthorization: [
+          { name: "from", type: "address" },
+          { name: "to", type: "address" },
+          { name: "value", type: "uint256" },
+          { name: "validAfter", type: "uint256" },
+          { name: "validBefore", type: "uint256" },
+          { name: "nonce", type: "bytes32" },
+        ],
+      },
+      domain: {
+        name: "USD Coin",
+        version: "2",
+        chainId: 5,  // chainId of network
+        verifyingContract: maraScanOperationsContract.address,
+      },
+      primaryType: "TransferWithAuthorization",
+      message: {
+        from: beneficiary.ethereumAccountAddress,
+        to: maraScanOperationsContract.address,
+        value: Math.pow(10, 6) * amount,  // amount
+        validAfter: 0,
+        validBefore: Math.floor(Date.now() / 1000) + 3600, // Valid for an hour
+        nonce: Web3.utils.randomHex(32),
+      },
+    };
 
     console.log(beneficiary)
   }
